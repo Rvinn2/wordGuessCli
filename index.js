a1656cac180cf9ee1d5ae433629ad27b7aed2e40
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-08-16 23:28:25 -0400

    index variables

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ var Word = require("./word.js");
 var inquirer = require("inquirer");
 
 var letterArray = "abcdefghijklmnopqrstuvwxyz";
+
+var randomColors = [
+    "Red",
+    "Blue",
+    "Green",
+    "Orange",
+    "Black",
+];
+
+var randomIndex = Math.floor(Math.random() * randomColors.length);
+
+var randomWord = randomColors[randomIndex];
+
+var computerWord = new Word(randomWord);
+
+var requireNewWord = false;
+var incorrectLetters = [];
+var correctLetters = [];
+
+

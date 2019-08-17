5939782f2f78568714b4a7d1962daa2b86d8d341
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-08-17 00:02:20 -0400

    updating variables for index

--- index.js
+++ index.js
@@ -25,3 +25,31 @@ var incorrectLetters = [];
 var correctLetters = [];
 
 
+var guessesLeft = 10;
+
+function theLogic(){
+    if(requireNewWord){
+        var randomIndex = Math.floor(Math.random() * randomColors.length);
+        randomWord = randomColors[randomIndex];
+
+        computerWord = new Word(randomWord);
+
+        requireNewWord = false;
+    }
+
+    var wordComplete = [];
+
+    if(wordComplete.includes(false)){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Choose a letter from A - Z ",
+                name: "userinput"
+            }
+           
+        ]).then(function(input){
+            
+        })
+    }
+}
+

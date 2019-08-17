cb88d045407f2fa0c659053a12dfb712d7375338
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-08-16 22:11:39 -0400

    require letter.js

--- word.js
+++ word.js
@@ -1,25 +1,2 @@
-function letter(value) {
+var Letter = require("./letter.js");
 
-    this.letter = value;
-    this.guessed = false;
-
-    this.toString = function() {
-
-        if (this.letter === " ") {
-            this.guessed = true;
-            return " ";
-        } else {
-            if (this.guessed === false) {
-            return "_";
-        } else {
-            return this.letter;
-        }
-    }
-    };
-
-    this.guess = function(guess) {
-        if (guess === this.letter) {
-            this.guessed = true;
-        }
-    };
-}
